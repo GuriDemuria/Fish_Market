251c11ceb66a85b93fde2744e34c14366f842f7e
GuriDemuria <gdemuria@spartaglobal.com>
2022-01-25 15:57:57 +0000

    With load to localdb and ec2db

--- fish_market.py
+++ fish_market.py
@@ -1,5 +1,7 @@
 import boto3
 import pandas as pd
+import csv
+import pymongo
 
 s3_client = boto3.client("s3")
 s3_resource = boto3.resource("s3")
@@ -9,25 +11,50 @@ prefix = 'python'
 
 def extract(bucket_name, prefix_name):
     bucket_contents = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix_name)
-    fish_market_csv = []
+    fish_market = []
     for page in bucket_contents['Contents']:
         if 'fish-market' in page['Key']:
             file = page['Key']
             file_csv = s3_client.get_object(Bucket=bucket_name, Key=file)
             df = pd.read_csv(file_csv['Body'])
-            fish_market_csv.append(df)
-    fish_market_csv = pd.concat(fish_market_csv, axis=0, ignore_index=True)
-    return fish_market_csv
+            fish_market.append(df)
+    fish_market = pd.concat(fish_market, axis=0, ignore_index=True)
+    return fish_market
 
-def transform(fish_market_csv):
-    transformed_fish_market_csv = fish_market_csv.groupby('Species').mean()
-    return transformed_fish_market_csv
+def transform(fish_market):
+    transformed_fish_market = fish_market.groupby('Species').mean()
+    return transformed_fish_market
 
-def load(transformed_fish_market_csv):
+def load(transformed_fish_market):
     with open('GuriD.csv', 'w') as csvfile:
-        transformed_fish_market_csv.to_csv(csvfile)
+        transformed_fish_market.to_csv(csvfile)
 
     s3_client.upload_file(Filename='GuriD.csv', Bucket=bucket, Key='Data26/fish/GuriD.csv')
 
+def load_to_localdb():
+    client = pymongo.MongoClient()
+    db = client['fishmarket']
 
-# load(transform(extract(bucket, prefix)))
+    db.drop_collection("averages")
+    db.create_collection('averages')
+    filename = "GuriD.csv"
+
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            db.averages.insert_one(line)
+
+
+def load_to_ec2():
+    client = pymongo.MongoClient("mongodb://18.194.99.217:27017/Sparta")
+    db = client['fishmarkets']
+
+    db.drop_collection("averages")
+    db.create_collection('averages')
+    filename = "GuriD.csv"
+
+    with open(filename, 'r') as data:
+        for line in csv.DictReader(data):
+            db.averages.insert_one(line)
+
+
+
